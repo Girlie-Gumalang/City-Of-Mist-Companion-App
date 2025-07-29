1adfd40b323020f76290cd72268d66767d6052c3
Girlie <girliegumalang@gmail.com>
2025-07-29 21:27:57 +0800

    Add features contact support activity

---
+++ app/src/main/java/com/jtdev/cs2a_group3/activities/ContactSupportActivity.java
@@ -0,0 +1,38 @@
+package com.jtdev.cs2a_group3.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.jtdev.cs2a_group3.R;
+import com.jtdev.cs2a_group3.fragments.AccountPage;
+
+public class ContactSupportActivity extends AppCompatActivity {
+
+    ImageView rb_contactSupp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_contact_support);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        rb_contactSupp = findViewById(R.id.return_button_contact_support);
+
+        rb_contactSupp.setOnClickListener(v -> {
+            finish();
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        });
+    }
+}
