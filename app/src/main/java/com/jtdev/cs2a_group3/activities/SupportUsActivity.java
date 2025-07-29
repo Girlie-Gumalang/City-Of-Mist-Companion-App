ce07a5d2f5d41834871eaf8e7822ec346b8e601a
Girlie <girliegumalang@gmail.com>
2025-07-29 21:30:31 +0800

    Add feature activity name support us

---
+++ app/src/main/java/com/jtdev/cs2a_group3/activities/SupportUsActivity.java
@@ -0,0 +1,37 @@
+package com.jtdev.cs2a_group3.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.jtdev.cs2a_group3.R;
+import com.jtdev.cs2a_group3.fragments.AccountPage;
+
+public class SupportUsActivity extends AppCompatActivity {
+    ImageView rb_supportUs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_support_us);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        rb_supportUs = findViewById(R.id.return_button_support_us);
+
+        rb_supportUs.setOnClickListener(v -> {
+            finish();
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        });
+    }
+}
